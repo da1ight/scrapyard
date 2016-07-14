b62d2c7ca7ee469dd7ad4e8c1bd02b1bdb8926d1
da1ight <st.osipov@yandex.ru>
2016-07-14 20:58:45 +0300

    Dynamic HAProxy backend configuration script

--- haproxy_dyn_b.sh
+++ haproxy_dyn_b.sh
@@ -6,6 +6,9 @@ cp /home/core/haproxy-config/haproxy.cfg /home/core/haproxy-config/haproxy.cfg.$
 #Delete previous backends
 sed -i /^"    server k8s_cluster_kubenode"/d /home/core/haproxy-config/haproxy.cfg
 
+#Export services list
+kubectl get --all-namespaces svc -o 'jsonpath={"Service:"}{" "}{"Namespace:"}{" "}{"NodePort:"}{"\n"}{range .items[?(@.spec.ports[*].nodePort)]}{.metadata.name}{" "}{.metadata.namespace}{" "}{.spec.ports[*].nodePort}{"\n"}{end}' | column -t | grep -v "kube-system" > /home/core/haproxy-config/svc.http
+
 #Get node port
 p=$(kubectl describe svc my-nginx | grep NodePort | grep -o '[0-9]*')
 
@@ -27,7 +30,7 @@ rm list.txt
 id=$(docker ps | grep haproxy | cut -c-12)
 docker stop $id
 
-#Start HAproxy cantainer with new config
-docker run -d -p 80:80 -v /home/core/haproxy-config:/usr/local/etc/haproxy/ haproxy:1.5
+#Start HAproxy container with new config
+docker run -d -p 80:80 -p 8090:8090 -v /home/core/haproxy-config:/usr/local/etc/haproxy/ haproxy:1.5
 
 #echo option httpchk HEAD /index.html HTTP/1.0 >> backendslist.txt
